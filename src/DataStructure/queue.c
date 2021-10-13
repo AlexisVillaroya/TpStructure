b19b2240cefe05edc9627230e49932ceebc1a1cc
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-10-13 11:52:44 +0200

    [feat] File

--- src/DataStructure/queue.c
+++ src/DataStructure/queue.c
@@ -4,3 +4,88 @@
  * and open the template in the editor.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+/**
+ * @brief Initilise l'index de la queue à zéro
+ * 
+ * @param
+ * 
+ * */
+
+void init_queue(Queue *s){
+    s->index = 0;
+}
+
+/**
+ * @brief Rajoute un élément à la queue
+ * 
+ * @param
+ * @param
+ * 
+ * */
+
+void enqueue(Queue *q, float value){
+    int i;
+    if(q->data[q->index] != 0){
+        for(i=q->index; i != 0; i++){
+            q->data[i-1] = value;
+        }
+    }else{
+        q->data[0] = value;
+    }
+}
+
+/**
+ * @brief Renvoie le
+ * 
+ * @param
+ * 
+ * */
+
+float dequeue(Queue *q){
+    float element = 0;
+
+    if(q->data[0] != 0){
+        element = q->data[q->index-1];
+        q->index++;
+    }
+    return element;
+}
+
+/**
+ * @brief 
+ * 
+ * @param
+ * 
+ * */
+
+bool is_queue_empty(Queue *q){
+    return q->index == 0;
+}
+
+/**
+ * @brief 
+ * 
+ * @param
+ * 
+ * */
+
+float front(Queue *q){
+    return q->data[q->index - 1];
+}
+
+/**
+ * @brief 
+ * 
+ * @param
+ * 
+ * */
+
+void clear(Queue *q){
+    for(int i = 0; i < 100; i++){
+        q->data[i] = 0;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "DataStructure/stack.h"
+#include "DataStructure/queue.h"
 
-int main()
-{
-	printf("Hello World!\n");
+float test(int a) {
+    //errno = a;
+    assert(a == 5);
+    //int errnum = errno;
+    //fprintf(stderr, "Value of errno: %d\n", errnum);
+    perror("Error printed by perror");
+    //fprintf(stderr, "Error opening file: %s\n", strerror(errnum));
 
-	return (0);
+}
+
+int main(int argc, char** argv) {
+    test_queue();
+    return (EXIT_SUCCESS);
+}
+
+void test_queue(){
+	Queue q;
+    clear(&q);
+    init_queue(&q);
+    assert(q.index == 0);
+    enqueue(&q, 254);
+    assert(q.data[0] == 254);
+    enqueue(&q, 15);
+    //assert(q.data[0] == 15);
+
+    for(int i=100; i > 0; i--){
+        printf("%.01f ", q.data[i]);
+    }
+
+    printf("%.01f ", q.data[0]);
+    printf("%.01f\n", q.data[1]);
+    dequeue(&q);
+    assert(q.index == 1);
+    printf("%.01f\n", q.data[0]);
+    printf("Tous c'est bien passé !\n");	
 }
