24cd83a3d81ac35c0e1f555f2aa344a354860e3c
Roman Lecomte <romanlecomte@gmail.com>
2021-10-15 09:09:59 +0200

    [ajout] Test unitaires pile (stack)

--- src/main.c
+++ src/main.c
@@ -8,7 +8,10 @@ void test_stack(Stack *s);
 void test_queue();
 
 int main(int argc, char** argv) {
-    test_queue();
+    //test_queue();
+    float a=0.0f;
+    Stack *s = malloc(sizeof(Stack));
+    test_stack(s);
     return (EXIT_SUCCESS);
 }
 
@@ -37,13 +40,24 @@ void test_queue(){
     printf("Tout s'est bien passé !\n");
 }	
 
-// void test_stack(Stack *s){
-// 	init_stack(s);
-// 	push(s, 5.0);
-// 	pop(s);
-// 	is_stack_empty(s);
-// 	peek(s);
-// 	dup(s);
-// 	swap(s);
-// 	cls(&s);
-// }
+void test_stack(Stack *s){
+	init_stack(s);
+    assert(s->index == -1);
+	push(s, 5.0f);
+	push(s, 52.3f);
+    assert(s->data[s->index-1] == 5.0f);
+    assert(s->data[s->index] == 52.3f);
+	pop(s);
+    assert(s->data[s->index] == 5.0);
+	is_stack_empty(s);
+    assert(is_stack_empty(s) == 0);
+
+    assert(peek(s) == 5.0);
+	dup(s);
+    assert(peek(s) == s->data[s->index-1]);
+    push(s, 52.3);
+	swap(s);
+    assert(peek(s) == 5.0);
+	cls(s);
+    assert(is_stack_empty(s) == 1);
+}
